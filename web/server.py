28b8ce03bbe79cbc43ee1a5d6de1ef7504c40f39
Sasha <scratalex@gmail.com>
2013-05-06 02:47:28 +0400

    web search permutations

--- web/server.py
+++ web/server.py
@@ -1,5 +1,6 @@
 from flask import *
 from db import *
+from itertools import permutations
 
 app = Flask(__name__)
 
@@ -8,15 +9,18 @@ app = Flask(__name__)
 def index(query=None):
     if not query:
         return "Welcome to fstate - searc engine for particle physics!"
-    
-    results = fstates.find({'fstate': query.split(' ')}).sort("branching", -1)
-    final_results = []
 
+    query = [x for x in query.split(' ') if x != '']
+    query_permutations = list(permutations(query, len(query)))
+
+    results = fstates.find({"fstate": {"$in": query_permutations}}).sort("branching", -1)
+    
+    final_results = []
 
     for r in results:
         r['scheme'] = [decays.find_one({"decay_id" : x, }) for x in r['scheme']]
         final_results.append(r)
-    
+
     return render_template('index.html', query=query, results=final_results)
 
 if __name__ == "__main__":
