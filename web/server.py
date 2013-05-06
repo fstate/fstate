9a021cc329379af0eba674c9daf91b473a6d2879
Sasha <scratalex@gmail.com>
2013-05-07 00:08:18 +0400

    Neat web interface

--- web/server.py
+++ web/server.py
@@ -5,10 +5,11 @@ from itertools import permutations
 app = Flask(__name__)
 
 @app.route("/")
-@app.route("/<query>")
-def index(query=None):
+def index():
+    query = request.args.get('query')
+    
     if not query:
-        return "Welcome to fstate - searc engine for particle physics!"
+        return render_template('index.html')
 
     query = [x for x in query.split(' ') if x != '']
     query_permutations = list(permutations(query, len(query)))
@@ -16,7 +17,10 @@ def index(query=None):
     results = fstates.find({"fstate": {"$in": query_permutations}}).sort("branching", -1)
 
     print "Found ", results.count()
-    return render_template('index.html', query=query, results=results)
+    return render_template('results.html', query=query, results=results)
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
