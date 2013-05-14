d9dea81b0009fd5e86a97967d5e297d19a1b6462
Sasha B <a.baranov@cern.ch>
2013-05-14 23:23:04 +0400

    .json and .txt search results

--- web/server.py
+++ web/server.py
@@ -2,8 +2,17 @@ from flask import *
 from db import *
 from itertools import permutations
 from datetime import datetime
+from printer import TablePrinter
 
 app = Flask(__name__)
+
+
+def do_search(query):
+    return list(fstates.find(
+        {"fstate": {"$all": query, "$size": len(query)}}, 
+        {'_id': False})
+    )
+
 
 @app.route("/")
 def index():
@@ -16,17 +25,53 @@ def index():
 
     start = datetime.now()
 
-    results = list(fstates.find({"fstate": {"$all": query, "$size": len(query)}}))
+    results = sorted(do_search(query), key=lambda x: -x['branching'][0])
 
     end = datetime.now()
-    
-    results = sorted(results, key=lambda x: -x['branching'][0])
     
 
     print 'Time for query "{}" - {}'.format(request.args.get('query'), end - start)
     
     return render_template('results.html', query=" ".join(query), results=results, timing=(end - start))
 
+
+@app.route('/<query>.json')
+def json(query):
+    if not query:
+        return redirect('/')
+
+    query = [x for x in query.split(' ') if x != '']
+
+    start = datetime.now()
+    result = sorted(do_search(query), key=lambda x: -x['branching'][0])
+    end = datetime.now()
+
+    
+    return Response(result, mimetype='application/json')
+
+
+format = [
+    ('Branching',       'branching',   15),
+    ('Scheme',          'scheme',       120),
+]
+
+@app.route('/<query>.txt')
+def txt(query):
+    if not query:
+        return redirect('/')
+
+    query = [x for x in query.split(' ') if x != '']
+
+    start = datetime.now()
+
+    result = sorted(do_search(query), key=lambda x: -x['branching'][0])
+
+    end = datetime.now()
+
+    for r in result:
+        r['branching'] = "%0.4g" % r['branching'][0]
+    
+    return Response(TablePrinter(format, ul='-')(result), mimetype='text/plain')
 
 if __name__ == "__main__":
     app.run(debug=True)
