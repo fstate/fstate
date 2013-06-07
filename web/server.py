f344461b9d9a48c3a2c747a1033ca2427fb175c8
Sasha B <a.baranov@cern.ch>
2013-06-07 15:34:38 +0400

    Advanced(with accounting of permutations) caching with memcached.

--- web/server.py
+++ web/server.py
@@ -1,5 +1,5 @@
 from flask import *
-from flask.ext.cache import Cache
+import pylibmc
 from db import *
 from itertools import permutations
 from datetime import datetime
@@ -8,28 +8,41 @@ from printer import TablePrinter
 from json import dumps as json_dump
 
 app = Flask(__name__)
-cache = Cache()
-cache.init_app(app, config={'CACHE_TYPE': 'simple'})
+mc = pylibmc.Client(["127.0.0.1"], binary=True,
+                     behaviors={"tcp_nodelay": True,
+                                "ketama": True})
+
+def cache_key(query):
+    return str(" ".join(query))
 
 
-@cache.memoize()
 def do_search(query):
+    # Set conversion is done for duplicate removing
+    query_permutations = list(set(permutations(query, len(query))))
+    
+    # Cache check
+    for q in query_permutations:
+        if cache_key(q) in mc:
+            return mc[cache_key(q)]
+
     if len(query) == len(set(query)):
         # Search without duplicates
-        return list(fstates.find(
+        result = sorted(list(fstates.find(
             {"fstate": {
                         "$all": query, 
                         "$size": len(query)}}, 
-            {'_id': False}))
+            {'_id': False})),  key=lambda x: -x['branching'][0])
     else:
-        # convertion to set is done for removing duplicates.
-        query_permutations = list(set(permutations(query, len(query))))
- 
-        return sorted(list(
+        # Duplicates in query
+        result = sorted(list(
         fstates.find(
              {"fstate": {"$in": query_permutations}}, 
              {'_id': False})
         ), key=lambda x: -x['branching'][0])
+
+    mc[cache_key(query)] = result
+    print "Memcached key {} value: \n\n{}\n\n{}".format(str(query), result, mc[cache_key(query)])
+    return result
     
 
 
@@ -72,7 +85,7 @@ def json(query):
     query = [x for x in query.split(' ') if x != '']
 
     start = datetime.now()
-    result = sorted(do_search(query), key=lambda x: -x['branching'][0])
+    result = do_search(query)
     end = datetime.now()
 
     
