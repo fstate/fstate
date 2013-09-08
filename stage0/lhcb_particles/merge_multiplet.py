b2c21f39b824914cf58bcefb618fec3af09daf85
ilyakomarov <ilya.komarov@epfl.ch>
2013-09-08 15:46:26 +0200

    merger

--- stage0/lhcb_particles/merge_multiplet.py
+++ stage0/lhcb_particles/merge_multiplet.py
@@ -13,10 +13,20 @@ def merge_multiplet(list_of_multiplets):
         Creae new multiplet with given name.
         Don't forget to remove these old multiplets from the list of the multipets.
     """
-
     particle_list = []
     for m in list_of_multiplets:
-        for p in multiplets[m]:
+        for p in multiplets[m]["particles"]:
             particle_list.append(p)
         del multiplets[m]
-    multiplets[list_intersection(list_of_multiplets)]=particle_list
+    mlt = {"particles":particle_list,"antimultiplet":list_intersection(list_of_multiplets)}
+    multiplets[list_intersection(list_of_multiplets)]=mlt
+
+def merge():
+    for name1, m1 in multiplets.iteritems():
+        for name2, m2 in multiplets.iteritems():
+            if m1["antimultiplet"] == name1:
+                if m2["antimultiplet"] == name2:
+                    if len(m1)+len(m2)==3:
+                        if ( m1 + "0" == m2) or (m2 + "0" == m1):
+                            list=[name1, name2]
+                            merge_multiplet(name1, name2)
