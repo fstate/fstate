47e061324fa486249851dc06f0fa9bec7f84955e
Sasha B <a.baranov@cern.ch>
2014-08-03 03:48:01 +0400

    Proper fixes in stage1

--- stage1/fixes/multiparticles.py
+++ stage1/fixes/multiparticles.py
@@ -6,19 +6,11 @@ def multiparticle_fix(decay_str):
         Fixes things like 4pi+ to pi+ pi+ pi+ pi+
     """
 
-    #def repl(m):
-    #    num = int(m.group(1))
-    #    return num * (m.group(2) + m.group(3) + " ")
-    #return re.sub(r'(\d+)(pi|mu|e|K)([{0,+,-}]*)^\d', repl, decay_str)
-
     def repl(m):
-        num = int(m.group(2))
-        multip = m.group(3)
-        if multip[0] == "(":
-            multip = multip[1::-2]+" "
-        return m.group(1) + num * multip + m.group(4)
+        num = int(m.group(1))
+        return " ".join([m.group(2)] * num)
 
-    prog = re.compile(r'(.*?\s)(\d)(\(.*?\)\s|[a-zA-Z].*?\s)(.*)')
+    prog = re.compile(r'(\d)([a-zA-Z]{1,2}[\+\-\0])')
 
     while True:
         decay_str = re.sub(prog, repl, decay_str)
